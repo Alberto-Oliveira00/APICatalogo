1d7b84b29b5bc05a20fb1808753568cb4cf7e6bd
Alberto Oliveira <alberto.olv00@gmail.com>
2025-07-29 09:17:22 -0300

    update Repository

--- APICatalogo/Repositories/ProdutoRepository.cs
+++ APICatalogo/Repositories/ProdutoRepository.cs
@@ -1,64 +1,63 @@
 ﻿using APICatalogo.Context;
 using APICatalogo.Models;
 
-namespace APICatalogo.Repositories
+namespace APICatalogo.Repositories;
+
+public class ProdutoRepository : IProdutoRepository
 {
-    public class ProdutoRepository : IProdutoRepository
+    private readonly AppDbContext _context;
+
+    public ProdutoRepository(AppDbContext context)
     {
-        private readonly AppDbContext _context;
+        _context = context;
+    }
 
-        public ProdutoRepository(AppDbContext context)
+    public IQueryable<Produto> GetProdutos()
+    {
+        return _context.Produtos;
+    }
+    public Produto GetProduto(int id)
+    {
+        var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+        if (produto is null)
+            throw new InvalidOperationException("Produto é null");
+
+        return produto;
+    }
+    public Produto Create(Produto produto)
+    {
+        if (produto is null)
+            throw new InvalidOperationException("Produto é null");
+
+        _context.Produtos.Add(produto);
+        _context.SaveChanges();
+        return produto;
+    }
+    public bool Update(Produto produto)
+    {
+        if (produto is null)
+            throw new InvalidOperationException("Produto é null");
+
+        if(_context.Produtos.Any(p => p.ProdutoId == produto.ProdutoId))
         {
-            _context = context;
-        }
-
-        public IQueryable<Produto> GetProdutos()
-        {
-            return _context.Produtos;
-        }
-        public Produto GetProduto(int id)
-        {
-            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
-
-            if (produto is null)
-                throw new InvalidOperationException("Produto é null");
-
-            return produto;
-        }
-        public Produto Create(Produto produto)
-        {
-            if (produto is null)
-                throw new InvalidOperationException("Produto é null");
-
-            _context.Produtos.Add(produto);
+            _context.Produtos.Update(produto);
             _context.SaveChanges();
-            return produto;
+            return true;
         }
-        public bool Update(Produto produto)
+        return false;
+    }
+
+    public bool Delete(int id)
+    {
+        var produto = _context.Produtos.Find(id);
+
+        if(produto is not null)
         {
-            if (produto is null)
-                throw new InvalidOperationException("Produto é null");
-
-            if(_context.Produtos.Any(p => p.ProdutoId == produto.ProdutoId))
-            {
-                _context.Produtos.Update(produto);
-                _context.SaveChanges();
-                return true;
-            }
-            return false;
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+            return true;
         }
-
-        public bool Delete(int id)
-        {
-            var produto = _context.Produtos.Find(id);
-
-            if(produto is not null)
-            {
-                _context.Produtos.Remove(produto);
-                _context.SaveChanges();
-                return true;
-            }
-            return false;
-        }
+        return false;
     }
 }
