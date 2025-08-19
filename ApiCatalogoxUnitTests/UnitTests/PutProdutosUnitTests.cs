7d19762ca98356fde40dfb413a5a3404fb321b0f
Alberto Oliveira <alberto.olv00@gmail.com>
2025-08-19 17:45:48 -0300

    Testes para Put Produtos

---
+++ ApiCatalogoxUnitTests/UnitTests/PutProdutosUnitTests.cs
@@ -0,0 +1,65 @@
+﻿using APICatalogo.Controllers;
+using APICatalogo.DTOs;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoxUnitTests.UnitTests;
+
+public class PutProdutosUnitTests : IClassFixture<ProdutosUnitTestController>
+{
+    private readonly ProdutosController _controller;
+
+    public PutProdutosUnitTests(ProdutosUnitTestController controller)
+    {
+        _controller = new ProdutosController(controller.repository, controller.mapper);
+    }
+
+    [Fact]
+    public async Task PutProduto_Return_OkResult()
+    {
+        //Arrange
+        var prodId = 14;
+
+        var updateProdutoDto = new ProdutoDTO
+        {
+            ProdutoId = prodId,
+            Nome = "Produto Atualizado",
+            Descricao = "Descrição do Produto Atualizado",
+            ImagemUrl = "imagematualizada.jpg",
+            CategoriaId = 2
+        };
+
+        //Act
+        var result = await _controller.Put(prodId, updateProdutoDto) as ActionResult<ProdutoDTO>;
+
+        //Assert
+        result.Should().NotBeNull();
+        result.Result.Should().BeOfType<OkObjectResult>();
+    }
+
+    [Fact]
+    public async Task PutProduto_Return_BadRequest()
+    {
+        //Arrange
+        var prodId = 999;
+        var updateProdutoDto = new ProdutoDTO
+        {
+            ProdutoId = 14,
+            Nome = "Produto Inexistente",
+            Descricao = "Descrição do Produto Inexistente",
+            ImagemUrl = "imageminexistente.jpg",
+            CategoriaId = 2
+        };
+
+        //Act
+        var data = await _controller.Put(prodId, updateProdutoDto);
+
+        //Assert
+        data.Result.Should().BeOfType<BadRequestObjectResult>().Which.StatusCode.Should().Be(400);
+    }
+}
--- ApiCatalogoxUnitTests/obj/Debug/net8.0/ApiCatalogoxUnitTests.AssemblyInfo.cs
+++ ApiCatalogoxUnitTests/obj/Debug/net8.0/ApiCatalogoxUnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiCatalogoxUnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb9c5b7e7103fba86cc392fb4837afa548b2d6f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37faad83bd67a496a93ec63bbc7bff8e98d9adb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiCatalogoxUnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiCatalogoxUnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
